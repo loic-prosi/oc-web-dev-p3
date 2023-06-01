e78fa62de16da78f76595adbb848cc5760289e25
Loïc Prosi <loicprosi@gmail.com>
2023-06-01 18:18:35 +0200

    refactor: works filters

--- FrontEnd/assets/scripts/categories.js
+++ FrontEnd/assets/scripts/categories.js
@@ -1,8 +1,42 @@
-export function createCategoryElement(category) {
+import { createWorkElement } from "./works.js";
+
+export function getWorksCategories(works) {
+  const categories = works.map((work) => work.category);
+
+  let uniqueCategories = categories.filter((value, index) => {
+    const _value = JSON.stringify(value);
+    return (
+      index ===
+      categories.findIndex((obj) => {
+        return JSON.stringify(obj) === _value;
+      })
+    );
+  });
+  // New category "0" for displaying all works ("All" button)
+  uniqueCategories.unshift({ id: "0", name: "Tous" });
+
+  return uniqueCategories;
+}
+
+export function createCategoryButtonElement(category, works) {
   const categoryElement = document.createElement("button");
   categoryElement.className = "portfolio__filters__button";
   categoryElement.innerText = category.name;
-  categoryElement.setAttribute("data-category", category.id);
+  categoryElement.addEventListener("click", async function () {
+    let worksFiltered = works.filter((work) => {
+      return work.category.id === parseInt(category.id);
+    });
+    // If clicked filter is "all" use the initial works array
+    if (category.id === "0") {
+      worksFiltered = works;
+    }
+    // Refresh gallery with filtered works
+    const galleryElement = document.querySelector(".portfolio__gallery");
+    galleryElement.innerHTML = "";
+    worksFiltered.forEach((work) => {
+      createWorkElement(work);
+    });
+  });
 
   const filtersElement = document.querySelector(".portfolio__filters");
   filtersElement.appendChild(categoryElement);
--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,5 +1,8 @@
 import { getAllWorks, createWorkElement } from "./works.js";
-import { createCategoryElement } from "./categories.js";
+import {
+  getWorksCategories,
+  createCategoryButtonElement
+} from "./categories.js";
 
 async function renderElements() {
   const works = await getAllWorks();
@@ -8,71 +11,10 @@ async function renderElements() {
     createWorkElement(work);
   });
 
-  const categories = works.map((work) => work.category);
-  const uniqueCategories = categories.filter((value, index) => {
-    const _value = JSON.stringify(value);
-    return (
-      index ===
-      categories.findIndex((obj) => {
-        return JSON.stringify(obj) === _value;
-      })
-    );
-  });
+  const categories = getWorksCategories(works);
 
-  uniqueCategories.forEach((category) => {
-    createCategoryElement(category, works);
-  });
-
-  const filterButtonCategory0 = document.querySelector("[data-category='0']");
-  filterButtonCategory0.addEventListener("click", async function (event) {
-    const categoryId = event.target.dataset.category;
-    const worksFiltered = works.filter((work) => {
-      return work.category.id !== parseInt(categoryId);
-    });
-    const galleryElement = document.querySelector(".portfolio__gallery");
-    galleryElement.innerHTML = "";
-    worksFiltered.forEach((work) => {
-      createWorkElement(work);
-    });
-  });
-
-  const filterButtonCategory1 = document.querySelector("[data-category='1']");
-  filterButtonCategory1.addEventListener("click", async function (event) {
-    const categoryId = event.target.dataset.category;
-    const worksFiltered = works.filter((work) => {
-      return work.category.id === parseInt(categoryId);
-    });
-    const galleryElement = document.querySelector(".portfolio__gallery");
-    galleryElement.innerHTML = "";
-    worksFiltered.forEach((work) => {
-      createWorkElement(work);
-    });
-  });
-
-  const filterButtonCategory2 = document.querySelector("[data-category='2']");
-  filterButtonCategory2.addEventListener("click", async function (event) {
-    const categoryId = event.target.dataset.category;
-    const worksFiltered = works.filter((work) => {
-      return work.category.id === parseInt(categoryId);
-    });
-    const galleryElement = document.querySelector(".portfolio__gallery");
-    galleryElement.innerHTML = "";
-    worksFiltered.forEach((work) => {
-      createWorkElement(work);
-    });
-  });
-
-  const filterButtonCategory3 = document.querySelector("[data-category='3']");
-  filterButtonCategory3.addEventListener("click", async function (event) {
-    const categoryId = event.target.dataset.category;
-    const worksFiltered = works.filter((work) => {
-      return work.category.id === parseInt(categoryId);
-    });
-    const galleryElement = document.querySelector(".portfolio__gallery");
-    galleryElement.innerHTML = "";
-    worksFiltered.forEach((work) => {
-      createWorkElement(work);
-    });
+  categories.forEach((category) => {
+    createCategoryButtonElement(category, works);
   });
 }
 
