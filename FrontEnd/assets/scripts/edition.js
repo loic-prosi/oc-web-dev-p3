e1f4bbfd63104579f0ae330dab1643af6ed40f47
Loïc Prosi <loicprosi@gmail.com>
2023-06-08 18:56:09 +0200

    feat: added edition mode in landing page

---
+++ FrontEnd/assets/scripts/edition.js
@@ -0,0 +1,93 @@
+function createEditionBarElement() {
+  let editionBarElement = document.createElement("div");
+  editionBarElement.className = "edition-bar";
+
+  let editionBarIconElement = document.createElement("img");
+  editionBarIconElement.className = "edition-bar__icon";
+  editionBarIconElement.src = "./assets/icons/edit-white.svg";
+
+  let editionBarTextElement = document.createElement("p");
+  editionBarTextElement.className = "edition-bar__text";
+  editionBarTextElement.innerText = "Mode édition";
+
+  let editionBarButtonElement = document.createElement("button");
+  editionBarButtonElement.className = "edition-bar__button";
+  editionBarButtonElement.innerText = "publier les changements";
+  editionBarButtonElement.addEventListener("click", function () {
+    location.reload();
+  });
+
+  let headerElement = document.querySelector("header");
+  headerElement.className = "edition-bar-offset";
+  headerElement.appendChild(editionBarElement);
+
+  editionBarElement.appendChild(editionBarIconElement);
+  editionBarElement.appendChild(editionBarTextElement);
+  editionBarElement.appendChild(editionBarButtonElement);
+}
+
+function createEditionButtonElement(
+  buttonContainerClassName,
+  buttonContainerParentSelector
+) {
+  let buttonContainer = document.createElement("div");
+  buttonContainer.className = buttonContainerClassName;
+
+  let button = document.createElement("button");
+  button.className = "edition-button";
+
+  let buttonImg = document.createElement("img");
+  buttonImg.className = "edition-button__icon";
+  buttonImg.src = "./assets/icons/edit-black.svg";
+
+  let buttonText = document.createElement("span");
+  buttonText.className = "edition-button__text";
+  buttonText.innerText = "modifier";
+
+  let buttonContainerParent = document.querySelector(
+    buttonContainerParentSelector
+  );
+
+  buttonContainerParent.appendChild(buttonContainer);
+  buttonContainer.appendChild(button);
+  button.appendChild(buttonImg);
+  button.appendChild(buttonText);
+}
+
+function createEditionButtonsElements() {
+  createEditionButtonElement(
+    "edition-button-container--introduction__figure",
+    ".introduction figure"
+  );
+  createEditionButtonElement(
+    "edition-button-container--introduction__article",
+    ".introduction article"
+  );
+  createEditionButtonElement(
+    "edition-button-container--portfolio",
+    ".portfolio__title"
+  );
+}
+
+function removePorfolioFilters() {
+  let portfolioFiltersParent = document.querySelector(
+    ".portfolio__filters"
+  ).parentElement;
+
+  let portfolioFilters = document.querySelector(".portfolio__filters");
+
+  portfolioFiltersParent.removeChild(portfolioFilters);
+}
+
+function updatePorfolioTitleStyles() {
+  let portfolioTitle = document.querySelector(".portfolio__title");
+  portfolioTitle.style.marginLeft = "132px";
+  portfolioTitle.style.marginBottom = "100px";
+}
+
+export function setEditionState() {
+  createEditionBarElement();
+  createEditionButtonsElements();
+  removePorfolioFilters();
+  updatePorfolioTitleStyles();
+}
